ecb4f057fe11267515a3f1f5a986a0a8e41dc6b7
Selva Prabhakaran <selva86@gmail.com>
2025-05-10 08:54:02 +0530

    mcp vs func call

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
+import ollama
+import yfinance as yf
+from pprint import pprint
+
+# Download ollama from https://ollama.com/ and run `ollama run llama3.2:1b` in terminal
+# Install yfinance with `pip install yfinance` or `uv add yfinance`
+
+def get_price(ticker):
+    """Get latest price for given ticker"""
+    stock = yf.Ticker(ticker)
+    return stock.history(period='1d')['close'].iloc[-1]
+
+response = ollama.chat(
+    'llama3.2',
+    messages=[{'role':'user', 'content': 'What is the stock price of Apple?'}],
+    tools=[get_price]
+)
+
+
 def main():
     print("Hello from 02-mcp-vs-function-calling!")
-
+    pprint(dict(response))
 
 if __name__ == "__main__":
     main()
